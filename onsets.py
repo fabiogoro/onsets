3403ec02bd43bccfd9ef7d83281d200777099585
fabio <fabiogo@outlook.com>
2017-11-29 14:37:47 -0200

    init

--- onsets.py
+++ onsets.py
@@ -4,8 +4,8 @@ import librosa
 
 def onsets(infile, fs):
     y, sr = librosa.load(infile, sr=fs, mono=True)
-    onset_env = librosa.onset.onset_strength(y=y, sr=fs, aggregate=np.median, fmax=1000, n_mels=13)
-    times = librosa.onset.onset_detect(y, fs, units='time', onset_envelope=onset_env, backtrack=True, wait=4, delta=0.001, pre_max=3, post_max=3, pre_avg=3, post_avg=3)
+    onset_env = librosa.onset.onset_strength(y=y, sr=fs) #, aggregate=np.median, fmax=1000, n_mels=13)
+    times = librosa.onset.onset_detect(y, fs, units='samples') #, onset_envelope=onset_env, backtrack=True, wait=4, delta=0.001, pre_max=3, post_max=3, pre_avg=3, post_avg=3)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
